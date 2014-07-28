baf104d1d5d69838d9ac4a7610f42c7f2739fbcd
Jasin Yip <yejunxing@gmail.com>
2014-07-28 21:43:22 +0800

    导航栏效果bug修复

--- js/main.js
+++ js/main.js
@@ -1,10 +1,22 @@
 $(function(){
 	$(window).resize();
-	var windowHeight = $(window).height();
+	var navSpace = $("header").offset().top;
 	$("#block-nav").css("z-index", 1);
 
 	// 导航条固定顶部
-	$("#block-nav").pin();
+	//$("#block-nav").pin();
+	//$("#block-nav").stickUp();
+	$(document).scroll( function() {
+	  var topSpace = $(document).scrollTop();
+	  if (topSpace>=navSpace){
+	    $("#block-nav").css("position", "fixed");
+	    $("#block-intro").css("margin-top", "52px");
+	  }else{
+	    $("#block-nav").css("position", "relative");
+	    $("#block-intro").css("margin-top", "0");
+	  }
+	});
+
 
 	//平滑滚动导航
 	$('nav a, #logo').bind('click',function(event){
@@ -20,12 +32,9 @@ $(function(){
 		$(this).css("background", "url(img/"+ $(this).attr('id').split('-')[1].toString() +".png) no-repeat center");
 	});
 
-
-
 });
 
 $(window).resize(function(){ 
-	$("#block-nav").pin();
 	$("#block-firstPage").css("height", $(window).height());
 	$('.blockTitle').css("top", ($(window).height() - $('.blockTitle').outerHeight())/2 ); 
 }); 
